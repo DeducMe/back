344b25aec75e46b918c04074b0ed23d33a61d5ca
DeducMe <deducme@gmail.com>
2021-10-30 02:51:35 +0300

    gitignore doesnt see 'data' folder hmmm

--- handlers/workWithJson.js
+++ handlers/workWithJson.js
@@ -1,9 +1,12 @@
 import * as fs from "fs";
 
 export async function updateOrganizationsJson(data) {
-  await fs.writeFileSync("./data/dataTest.json", JSON.stringify(data, 0, 2));
+  await fs.writeFileSync(
+    "./jsonData/dataTest.json",
+    JSON.stringify(data, 0, 2)
+  );
 }
 
 export async function getOrganizationsJson() {
-  return JSON.parse(fs.readFileSync("./data/myjsonfile.json", "utf-8"));
+  return JSON.parse(fs.readFileSync("./jsonData/myjsonfile.json", "utf-8"));
 }
